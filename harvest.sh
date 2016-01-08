15b7c206b5b6c5689177623980815c7e371ba85e
sallamander <ssall@alumni.nd.edu>
2016-01-08 00:22:04 +0000

    harvest instructions

---
+++ harvest.sh
@@ -0,0 +1,6 @@
+# make sure you have installed oaiharvest already:
+# https://pypi.python.org/pypi/oaiharvest
+
+# this should work, though untested (I ran it from command line and manually made the directory)
+# mkdir /user/Ubuntu/arxiv-data
+# oai-harvest -f 2010-01-01 -u 2016-01-01 -d /user/Ubuntu/arxiv-data http://export.arxiv.org/oai2
