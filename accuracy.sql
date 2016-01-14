e69c68e40e4cdb5bffab52bfcd701c4fd8511325
sallamander <ssall@alumni.nd.edu>
2016-01-14 01:22:32 +0000

    tweaked accuracy reporting

--- accuracy.sql
+++ accuracy.sql
@@ -1,14 +1,13 @@
 WITH merged AS (
 	SELECT 
-		c.similarity, c.article_a, c.article_b, 
-		a1.subject s1, a2.subject s2 
-		from cache_w2v as c 
+		c.article_a, c.article_b, 
+		a1.subject s1, a2.subject s2
+		from cache_d2v as c 
 		LEFT JOIN articles a1 
 		ON c.article_a = a1.index 
 		LEFT JOIN articles a2 
 		ON c.article_b = a2.index 
-		WHERE similarity > 0.6
-	) 
-	SELECT count(*)
-	FROM merged 
-	WHERE s1 = s2
+	)
+SELECT count(*)
+FROM merged 
+WHERE s1 = s2
