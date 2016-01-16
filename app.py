5085285e8b7b0d6a3b58bd14cf315baae09eb3f3
Sepehr Sadighpour <sepehr125@gmail.com>
2016-01-16 02:37:15 -0800

    fixed messaging on analogy

--- app.py
+++ app.py
@@ -89,11 +89,13 @@ def find_analogy():
     
     unlike = request.args.get('unlike', '')
     unlike = [word.lower() for word in list(unlike) if word not in ('', '#')]
+    if not likes and not unlike:
+        return render_template("analogy.html", analogies='find')
     try:
         analogies = model.most_similar(positive=likes, negative=unlike)
         return render_template("analogy.html", analogies=analogies)
     except:
-        return render_template("analogy.html", analogies=[])
+        return render_template("analogy.html", analogies='notfound')
 
 
 
