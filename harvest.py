02263b19bade5c3d4b2eefa46c1c85f139ec0927
Sepehr Sadighpour <sepehr125@gmail.com>
2016-01-17 04:05:35 -0800

    Refactored writing XML to database to speed up

--- harvest.py
+++ harvest.py
@@ -1,5 +1,16 @@
-# make sure you have installed oaiharvest already (using py27):
-# https://pypi.python.org/pypi/oaiharvest
+"""
+Harvest metadata of ArXiv articles (including abstract)
+for given subject or date range
+from Open Archives Initiative (OAI).
+More information: http://arxiv.org/help/oa/index
 
-# then simply run:
+make sure you have installed oaiharvest already:
+https://pypi.python.org/pypi/oaiharvest
+"""
+
+
+# run this at the terminal:
 # oai-harvest -from 2016-01-01 -until 2016-01-07 http://export.arxiv.org/oai2
+
+# by default, the metadataPrefix is set to "oai_dc"
+# which specifies the XML format.
--- train.py
+++ train.py
@@ -10,25 +10,34 @@ class DocIterator(object):
     """
     gensim documentation calls this "streaming a corpus", which 
     lets us train without holding entire corpus in memory.
-    Doc2Vec requires two passes over the data, so it is necessary
-    to create this iterator object that it can call twice.
-    Here, we stream from a postgres database.
     """
-
     def __init__(self, conn):
         self.conn = conn
 
     def __iter__(self):
+        """
+        Doc2Vec requires two passes over the data, so it is necessary
+        to create this iterator object that it can call twice.
+        Here, we stream from a postgres database.
+        """
         with conn.cursor(cursor_factory=DictCursor) as cur:
-            # I'd really like to store names of databases, tables, columns
-            # in a central location, possibly a json file or pickled dict
-            # that various files can use.
+            # TODO: save names of table and database 
+            # to a central location. For now, db=arxive and table=articles
             cur.execute("SELECT * FROM articles;")
-            for index, title, authors, subject, abstract, pubdate, arxiv_id, subject_id in cur:
-                body = title + '. ' + abstract
+            for article in cur:
+                abstract = article['abstract'].replace('\n', ' ').strip()
+                # train on body, composed of title and abstract
+                body = article['title'] + '. ' 
+                body += abstract
+                # We want to keep some punctuation, as Word2Vec
+                # considers them useful context
                 words = re.findall(r"[\w']+|[.,!?;]", body)
+                # lowercase. perhaps lemmatize too?
                 words = [word.lower() for word in words]
-                tags = [index]
+                # document tag. Unique integer 'index' is good.
+                # can also add topic tag of form 
+                # 'topic_{subject_id}' to list
+                tags = [article['index']]
                 yield TaggedDocument(words, tags)
 
 
@@ -42,7 +51,10 @@ if __name__ == '__main__':
     n_cpus = multiprocessing.cpu_count()
     with psycopg2.connect(dbname=args.dbname) as conn:
         doc_iterator = DocIterator(conn)
-        model = Doc2Vec(documents=doc_iterator, workers=n_cpus)
+        model = Doc2Vec(documents=doc_iterator,
+            workers=n_cpus,
+
+            )
 
     model.save(args.path_to_model)
     print("Model can be found at %s"%args.path_to_model)
